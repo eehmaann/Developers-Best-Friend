566403c2a9b193cda782b839bff0b71427e56078
Eric Ehmann <eaehmann@gmail.com>
2016-10-26 23:05:40 -0400

    edited validation problems

--- resources/views/oldindex.blade.php
+++ resources/views/oldindex.blade.php
@@ -18,23 +18,23 @@
     <form method='POST' action='/users' id="user">
         {{ csrf_field() }}
         <label> Random User Generator</label>
-        <p><input type='number' name='peopleNumber' min="1", max="20">
-        <label for='peoplenumber'> How many users?</label></p>
-        <p> <input type="checkbox" name="address" value="yes"> <label for="address">Check to include address </label></p>
-        <p> <input type="checkbox" name="phone" value="yes"> <label for="phone">Check to include phone number </label></p>
-        <p> <input type="checkbox" name="position" value="yes"> <label for="position">Check to include job title </label></p>
-        <p> <input type="checkbox" name="age" value="yes"> <label for="age">Check to include age </label></p>
-        <p> <input type="checkbox" name="leaf" value="yes"> <label for="leaf">Check to use bird leaf </label></p>
-        <p> <input type="checkbox" name="highlight" value="yes"> <label for="highlight">Check to highlight </label></p>
+        <p><input type='number' name='peopleNumber' min="1" max="20">
+        <label><b> How many users?</b></label></p>
+        <p> <input type="checkbox" name="address" value="yes"> <label>Check to include address </label></p>
+        <p> <input type="checkbox" name="phone" value="yes"> <label>Check to include phone number </label></p>
+        <p> <input type="checkbox" name="position" value="yes"> <label>Check to include job title </label></p>
+        <p> <input type="checkbox" name="age" value="yes"> <label>Check to include age </label></p>
+        <p> <input type="checkbox" name="leaf" value="yes"> <label>Check to use bird leaf </label></p>
+        <p> <input type="checkbox" name="highlight" value="yes"> <label>Check to highlight </label></p>
         <input type='submit' value='Give me people!'>
     </form>
 	
 	<form method='POST' action='/lorem' id="lorem">
     	{{ csrf_field() }}
-    	<label> Lorem Ipsum Generator</label>
-    	<p><input type='number' name='paragraphsNumber'>
-    	<label for='paragraphsNumber'>How many paragraphs?</label> </p>
-    	<p> <input type="checkbox" name="leaf" value="yes"> <label for="leaf">Check to use bird leaf? </label></p>
+    	<label><b> Lorem Ipsum Generator</b></label>
+    	<p><input type='number' name='paragraphsNumber' min="1" max="90">
+    	<label>How many paragraphs?</label> </p>
+    	<p> <input type="checkbox" name="leaf" value="yes"> <label>Check to use bird leaf? </label></p>
    	 	<input type='submit' value='Give me Content!'>
 	</form>
 
